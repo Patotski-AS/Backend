ad065ce76593ec080aa477407c7cb4606d828a5a
pototskiy_as <7906241@gmail.com>
2023-03-22 18:37:41 +0300

    init: initial new project

---
+++ spring-kotlin-z/src/test/kotlin/com/example/springkotlinz/SpringKotlinZApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springkotlinz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinZApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
